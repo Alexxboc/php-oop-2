deb83d52442bf13dfe552f02acba569297afcd08
Alexxboc <boccardi.alessandro@gmail.com>
2022-05-26 15:38:57 +0200

    Create basic html layout

--- Models/Product.php
+++ Models/Product.php
@@ -1,19 +1,21 @@
 <?php
 
 class Product {
-    protected $name;
-    protected $description;
-    protected $price;
-    protected $discountPrice;
-    protected $discountAmount;
-    protected $isAvailable;
+    public $name;
+    public $description;
+    public $price;
+    public $discountPrice;
+    public $discountAmount;
+    public $isAvailable;
+    public $poster;
 
-    public function __construct(String $_name, String $_description, Float $_price, Float $_discountPrice, Int $_discountAmount, Bool $_isAvailable = true) {
+    public function __construct(String $_name, String $_description, Float $_price, Float $_discountPrice, Int $_discountAmount, String $_poster, Bool $_isAvailable = true) {
         $this->name = $_name;
         $this->description = $_description;
         $this->price = $_price;
         $this->discountPrice = $_discountPrice;
         $this->discountAmount = $_discountAmount;
         $this->isAvailable = $_isAvailable;
+        $this->poster = $_poster;
     }
 }
--- index.php
+++ index.php
@@ -16,25 +16,84 @@ public function functionName( MyClass $variable, String $variable2, Int $variabl
 
  -->
 
- <?php
+<?php
 
- require_once __DIR__ . "/Models/Product.php";
- require_once __DIR__ . "/Models/DogEating.php";
- require_once __DIR__ . "/Models/DogBed.php";
- require_once __DIR__ . "/Models/Frisbee.php";
- 
+require_once __DIR__ . "/Models/Product.php";
+require_once __DIR__ . "/Models/DogEating.php";
+require_once __DIR__ . "/Models/DogBed.php";
+require_once __DIR__ . "/Models/Frisbee.php";
+
 
 //  $mongeMediumAdult = new DogEating('Monge Medium Adult', 'Mangiare per cane adulto', 29.90, 26.91, 10);
 //  $geoFrisbeeTornado = new Frisbee('Geo frisbee Tornado', 'Frisbee di plastica', 15, 12, 20);
 //  $cucciaDomus = new DogBed('Cuccia Domus', 'Cuccia di legno', 59.90, 41.93, 30);
 
- $products = [
-    new DogEating('Monge Medium Adult', 'Mangiare per cane adulto', 29.90, 26.91, 10),
-    new Frisbee('Geo frisbee Tornado', 'Frisbee di plastica', 15, 12, 20),
-    new DogBed('Cuccia Domus', 'Cuccia di legno', 59.90, 41.93, 30)
+$products = [
+    new DogEating('Monge Medium Adult', 'Mangiare per cane adulto', 29.90, 26.91, 10, 'https://picsum.photos/id/237/400/300'),
+    new Frisbee('Geo frisbee Tornado', 'Frisbee di plastica', 15, 12, 20, 'https://picsum.photos/id/237/400/300'),
+    new DogBed('Cuccia Domus', 'Cuccia di legno', 59.90, 41.93, 30, 'https://picsum.photos/id/237/400/300')
 ];
 
- //var_dump($mongeMediumAdult, $geoFrisbeeTornado, $cucciaDomus);
+//var_dump($mongeMediumAdult, $geoFrisbeeTornado, $cucciaDomus);
 
- var_dump($products);
-    
+//var_dump($products);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Bootstrap cdn Libraries -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row row-cols-3">
+            <?php foreach ($products as $product) : ?>
+                <div class="col">
+                    <div class="card_product border border-light">
+                        <div class="img">
+                            <img src="<?= $product->poster; ?>" alt="<?= $product->name; ?>" class="img-fluid">
+                        </div>
+                        <!-- /.img -->
+
+                        <div class="card_body text-center">
+                            <h5 class="product_name">
+                            <?= $product->name; ?>
+                            </h5>
+                            <!-- /.product_name -->
+                            <div class="price">
+                                <span class="official_price me-2">
+                                <?= $product->price; ?>
+                                </span>
+                                <!-- /.official_price -->
+                                <span class="discount_price text-danger">
+                                <?= $product->discountPrice; ?>
+                                </span>
+                                <!-- /.discount_price -->
+                            </div>
+                            <!-- /.price -->
+                            <p class="description">
+                            <?= $product->description; ?>
+                            </p>
+                            <!-- /.description -->
+                        </div>
+                        <!-- /.card_body -->
+
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            <?php endforeach; ?>
+        </div>
+        <!-- /.row row-cols-3 -->
+    </div>
+</body>
+
+</html>
